8f514cffca58f917a315f3524bf18954b3cfd33c
limecoding <bum9516@gmail.com>
2026-03-19 22:56:24 +0900

    feat: 로깅 설정 추가

--- apps/api/src/main/java/com/limecoding/api/common/config/WebConfig.java
+++ apps/api/src/main/java/com/limecoding/api/common/config/WebConfig.java
@@ -1,12 +1,19 @@
 package com.limecoding.api.common.config;
 
+import com.limecoding.api.common.logging.LoggingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final LoggingInterceptor loggingInterceptor;
+
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -22,5 +29,11 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/api/**");
     }
 }
