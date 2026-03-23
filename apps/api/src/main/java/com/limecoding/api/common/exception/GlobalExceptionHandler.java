f59c289f64e26c7a1103d1a699e765a5f277adc3
limecoding <bum9516@gmail.com>
2026-03-24 00:30:19 +0900

    feat: ResourceNotFoundException 추가

--- apps/api/src/main/java/com/limecoding/api/common/exception/GlobalExceptionHandler.java
+++ apps/api/src/main/java/com/limecoding/api/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.limecoding.api.common.exception;
 
 import com.limecoding.api.common.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -16,6 +18,15 @@ public class GlobalExceptionHandler {
         ApiResponse<Void> error = ApiResponse.error(errorCode.getStatus(), errorCode.getMessage());
 
         return ResponseEntity.status(errorCode.getStatus()).body(error);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleResourceNotFoundException(ResourceNotFoundException e) {
+        log.warn("Resource not found: {}", e.getMessage());
+
+        ApiResponse<Void> error = ApiResponse.error(HttpStatus.NOT_FOUND, e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -28,6 +39,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+
+        log.error("Internal server error: {}", e.getMessage(), e);
+
         ApiResponse<Void> error = ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
